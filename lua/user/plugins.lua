ac3708ad5241df05abde3c19bacf6155037f35ce
Zargham Khan <zarghamkhan980@gmail.com>
2022-09-06 23:08:39 +0200

    add tree sitter

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -68,6 +68,11 @@ return packer.startup(function(use)
   }
   use { "junegunn/fzf", run = ":call fzf#install()" }
 
+  -- Treesitter
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  }
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then

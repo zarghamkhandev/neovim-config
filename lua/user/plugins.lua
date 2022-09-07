42606e1a9caf46afd8cfcf028daf867d09047cef
Zargham Khan <zarghamkhan980@gmail.com>
2022-09-07 12:52:12 +0200

    added autopairs and coment plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -45,6 +45,8 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
+  use "numToStr/Comment.nvim" -- Easily comment stuff
   use 'navarasu/onedark.nvim'
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
@@ -73,6 +75,7 @@ return packer.startup(function(use)
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
   }
+  use 'JoosepAlviste/nvim-ts-context-commentstring'
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then

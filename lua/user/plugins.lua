bf148cb915c4aa464e7c8fee474fa04307eed666
Zargham Khan <zarghamkhan980@gmail.com>
2022-09-08 23:43:50 +0200

    added toggle term

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -52,6 +52,7 @@ return packer.startup(function(use)
   use 'kyazdani42/nvim-tree.lua'
   use "akinsho/bufferline.nvim"
   use "moll/vim-bbye"
+  use "akinsho/toggleterm.nvim"
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
   use "hrsh7th/cmp-buffer" -- buffer completions

37810780d5ea6ea432d9601aca0b18be22ed0073
Zargham Khan <zarghamkhan980@gmail.com>
2022-09-10 21:42:13 +0200

    changed one dark theme

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -47,7 +47,7 @@ return packer.startup(function(use)
   use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
   use("windwp/nvim-autopairs") -- Autopairs, integrates with both cmp and treesitter
   use("numToStr/Comment.nvim") -- Easily comment stuff
-  use("navarasu/onedark.nvim")
+  use("monsonjeremy/onedark.nvim")
   use("kyazdani42/nvim-web-devicons") -- Nvim tree stuff
   use("kyazdani42/nvim-tree.lua")
   use("akinsho/bufferline.nvim")
@@ -58,6 +58,7 @@ return packer.startup(function(use)
   use("folke/which-key.nvim")
   use("goolord/alpha-nvim")
   use("antoinemadec/FixCursorHold.nvim") -- This is needed to fix lsp doc highlight
+
   -- cmp plugins
   use("hrsh7th/nvim-cmp") -- The completion plugin
   use("hrsh7th/cmp-buffer") -- buffer completions

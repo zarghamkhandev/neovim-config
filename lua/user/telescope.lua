a860ae7e3a6d76e865771d86cebb13c86830a43f
Zargham Ahmad Khan <61333365+zarghamkhandev@users.noreply.github.com>
2022-09-13 00:34:26 +0200

    Update telescope.lua

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -3,46 +3,76 @@ local present, telescope = pcall(require, "telescope")
 if not present then
   return
 end
+  local previewers = require "telescope.previewers"
+  local sorters = require "telescope.sorters"
+  local actions = require "telescope.actions"
 local options = {
   defaults = {
     path_display = { "truncate" },
-    prompt_prefix = "$ ",
-  },
-  pickers = {
-    find_files = {
-      find_command = { "fd", "-g", "--type", "f" },
-    },
-  },
-
-  extensions = {
-    ["zf-native"] = {
-      -- options for sorting file-like items
-      file = {
-        -- override default telescope file sorter
-        enable = true,
-
-        -- highlight matching text in results
-        highlight_results = true,
-
-        -- enable zf filename match priority
-        match_filename = true,
+      prompt_prefix = " ",
+      selection_caret = " ",
+      entry_prefix = "  ",
+      initial_mode = "insert",
+      selection_strategy = "reset",
+      sorting_strategy = "descending",
+      layout_strategy = "horizontal",
+      layout_config = {
+        width = 0.75,
+        preview_cutoff = 120,
+        horizontal = {
+          preview_width = function(_, cols, _)
+            if cols < 120 then
+              return math.floor(cols * 0.5)
+            end
+            return math.floor(cols * 0.6)
+          end,
+          mirror = false,
+        },
+        vertical = { mirror = false },
       },
-
-      -- options for sorting all other items
-      generic = {
-        -- override default telescope generic item sorter
-        enable = true,
-
-        -- highlight matching text in results
-        highlight_results = true,
-
-        -- disable zf filename match priority
-        match_filename = false,
+      vimgrep_arguments = {
+        "rg",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case",
+        "--hidden",
+        "--glob=!.git/",
+      },
+      file_ignore_patterns = {},
+      path_display = { shorten = 5 },
+      winblend = 0,
+      border = {},
+      borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+      color_devicons = true,
+      set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+  },
+    file_previewer = previewers.vim_buffer_cat.new,
+    grep_previewer = previewers.vim_buffer_vimgrep.new,
+    qflist_previewer = previewers.vim_buffer_qflist.new,
+    file_sorter = sorters.get_fuzzy_file,
+    generic_sorter = sorters.get_generic_fuzzy_sorter,
+     pickers = {
+      find_files = {
+      },
+      live_grep = {
+        --@usage don't include the filename in the search results
+        only_sort_text = true,
       },
     },
-  },
+    extensions = {
+      fzf = {
+        fuzzy = false, -- false will only do exact matching
+        override_generic_sorter = true, -- override the generic sorter
+        override_file_sorter = true, -- override the file sorter
+        case_mode = "ignore_case", -- or "ignore_case" or "respect_case"
+      },
+    },
 }
 
 telescope.setup(options)
 
-require("telescope").load_extension("zf-native")
+require("telescope").load_extension "fzf"
+

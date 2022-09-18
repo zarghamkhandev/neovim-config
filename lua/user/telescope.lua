f2a89976ccb36408a60df9595f9c147334bf271d
Zargham Khan <zarghamkhan980@gmail.com>
2022-09-18 16:20:39 +0200

    added keymap to close telescope

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -1,82 +1,85 @@
 local present, telescope = pcall(require, "telescope")
 
 if not present then
-  return
+	return
 end
-  local previewers = require "telescope.previewers"
-  local sorters = require "telescope.sorters"
-  local actions = require "telescope.actions"
+local previewers = require("telescope.previewers")
+local sorters = require("telescope.sorters")
+local actions = require("telescope.actions")
 local options = {
-  defaults = {
-    path_display = { "truncate" },
-      prompt_prefix = " ",
-      selection_caret = " ",
-      entry_prefix = "  ",
-      initial_mode = "insert",
-      selection_strategy = "reset",
-      sorting_strategy = "descending",
-      layout_strategy = "horizontal",
-      layout_config = {
-        width = 0.75,
-        preview_cutoff = 120,
-        horizontal = {
-          preview_width = function(_, cols, _)
-            if cols < 120 then
-              return math.floor(cols * 0.5)
-            end
-            return math.floor(cols * 0.6)
-          end,
-          mirror = false,
-        },
-        vertical = { mirror = false },
-      },
-      vimgrep_arguments = {
-        "rg",
-        "--color=never",
-        "--no-heading",
-        "--with-filename",
-        "--line-number",
-        "--column",
-        "--smart-case",
-        "--hidden",
-        "--glob=!.git/",
-      },
-      file_ignore_patterns = {},
-       path_display = function(opts, path)
-        local tail = require("telescope.utils").path_tail(path)
-        return string.format("%s    (%s)", tail, path)
-      end,
+	defaults = {
+		mappings = {
+			i = {
+				["<esc>"] = actions.close,
+			},
+		},
+		path_display = { "truncate" },
+		prompt_prefix = " ",
+		selection_caret = " ",
+		entry_prefix = "  ",
+		initial_mode = "insert",
+		selection_strategy = "reset",
+		sorting_strategy = "descending",
+		layout_strategy = "horizontal",
+		layout_config = {
+			width = 0.75,
+			preview_cutoff = 120,
+			horizontal = {
+				preview_width = function(_, cols, _)
+					if cols < 120 then
+						return math.floor(cols * 0.5)
+					end
+					return math.floor(cols * 0.6)
+				end,
+				mirror = false,
+			},
+			vertical = { mirror = false },
+		},
+		vimgrep_arguments = {
+			"rg",
+			"--color=never",
+			"--no-heading",
+			"--with-filename",
+			"--line-number",
+			"--column",
+			"--smart-case",
+			"--hidden",
+			"--glob=!.git/",
+		},
+		file_ignore_patterns = {},
+		path_display = function(opts, path)
+			local tail = require("telescope.utils").path_tail(path)
+			return string.format("%s    (%s)", tail, path)
+		end,
 
-      winblend = 0,
-      border = {},
-      borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-      color_devicons = true,
-      set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
-  },
-    file_previewer = previewers.vim_buffer_cat.new,
-    grep_previewer = previewers.vim_buffer_vimgrep.new,
-    qflist_previewer = previewers.vim_buffer_qflist.new,
-    file_sorter = sorters.get_fuzzy_file,
-    generic_sorter = sorters.get_generic_fuzzy_sorter,
-     pickers = {
-      find_files = {
-      },
-      live_grep = {
-        --@usage don't include the filename in the search results
-        only_sort_text = true,
-      },
-    },
-    extensions = {
-      fzf = {
-        fuzzy = false, -- false will only do exact matching
-        override_generic_sorter = true, -- override the generic sorter
-        override_file_sorter = true, -- override the file sorter
-        case_mode = "ignore_case", -- or "ignore_case" or "respect_case"
-      },
-    },
+		winblend = 0,
+		border = {},
+		borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+		color_devicons = true,
+		set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+	},
+	file_previewer = previewers.vim_buffer_cat.new,
+	grep_previewer = previewers.vim_buffer_vimgrep.new,
+	qflist_previewer = previewers.vim_buffer_qflist.new,
+	file_sorter = sorters.get_fuzzy_file,
+	generic_sorter = sorters.get_generic_fuzzy_sorter,
+	pickers = {
+		find_files = {},
+		live_grep = {
+			--@usage don't include the filename in the search results
+			only_sort_text = true,
+		},
+	},
+	extensions = {
+		fzf = {
+			fuzzy = false, -- false will only do exact matching
+			override_generic_sorter = true, -- override the generic sorter
+			override_file_sorter = true, -- override the file sorter
+			case_mode = "ignore_case", -- or "ignore_case" or "respect_case"
+		},
+	},
 }
 
 telescope.setup(options)
 
-require("telescope").load_extension "fzf"
-
+require("telescope").load_extension("fzf")

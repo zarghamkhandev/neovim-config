8c4b69584cf9f6ba5b490937a6a8c93c3f3d291d
Zargham Khan <zarghamkhan980@gmail.com>
2022-09-18 19:33:48 +0200

    change keymaps

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
 require("user.keymaps")
 require("user.options")
-
+require("user.comments")
 
 if vim.fn.exists("g:vscode") ~= 0 then
-    vim.cmd('source $HOME/.config/nvim/vscode/settings.vim')
-  return
+	vim.cmd("source $HOME/.config/nvim/vscode/settings.vim")
+	return
 end
 
 require("user.plugins")
@@ -14,7 +14,6 @@ require("user.cmp")
 require("user.lsp")
 require("user.treesitter")
 require("user.autopairs")
-require("user.comments")
 require("user.gitsigns")
 require("user.nvim-tree")
 require("user.bufferline")
